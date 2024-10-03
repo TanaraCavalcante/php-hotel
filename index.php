f7a09386365c68d97a34a620f9b69555b843669a
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-10-03 15:50:43 -0400

    style

--- index.php
+++ index.php
@@ -47,20 +47,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hotel</title>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/33f70ca49a.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <main>
-        <section>
-             <ul>
-                <?php  foreach($hotels as $hotel) { ?>
-                    <li>  <?php echo $hotel["name"] ?>   </li>
-                    <li>  <?php echo $hotel["description"] ?>   </li>
-                    <li>  <?php echo $hotel["parking"] ?>   </li>
-                    <li>  <?php echo $hotel["vote"] ?>   </li>
-                    <li>  <?php echo $hotel["distance_to_center"] ?> kms  </li>
-                <? } ?>
-             </ul>
-        </section>
+    <main class="container">
+        <h2 class="mb-5">Hotel List</h2>
+            <section class="my-3">
+                <div>
+                    <ul class="list-unstyled">
+                        <?php  foreach($hotels as $hotel) { ?>
+                            <li class="card-title fs-5">  <?php echo $hotel["name"] ?>   </li>
+                            <li class="card-subtitle mb-2 text-muted">  <?php echo $hotel["description"] ?>   </li>
+                            <li class="card-text"> Parking: <?php echo $hotel["parking"] ?>   </li>
+                            <li class="card-text">Vote:  <?php echo $hotel["vote"] ?> <i class="fa-solid fa-star" style="color: #FFD43B;"></i> </li>
+                            <li class="card-text mb-3">Distance to center:  <?php echo $hotel["distance_to_center"] ?> kms  </li>
+                           <hr>
+                        <? } ?>
+                    </ul>
+                </div>
+            </section>
     </main>
     
 </body>
